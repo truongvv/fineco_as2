5c129d6cd6137448fd9d433fc7ccdcd9d81e2481
cvbilt <tampilprimananda@Tampils-MacBook-Pro.local>
2019-09-30 00:05:19 +1000

    latest update

--- charles_eda.R
+++ charles_eda.R
@@ -88,4 +88,132 @@ formattable(dictionary1, align = c("l","l","r"), list(
 
 }
 
-##### EDA pre FE #####
+##### EDA pre Featre Engineering #####
+{
+Combi_df <- data.frame(date=index(Combi), coredata(Combi))
+Combi_df$date <- as.Date(Combi_df$date)
+colnames(Combi_df)
+#Plotting rba_cash_rate
+ggplot(Combi_df, aes(date, rba_cash_rate)) + geom_line() +
+  xlab("Date") + ylab("RBA Cash Rate") + ggtitle("Cash Rate Target as announced by the RBA") +
+  annotate(geom="text", x=as.Date("2012-01-01"), y=6,
+           label="Lehman Brothers filed for bankruptcy") +
+  annotate(geom="text", x=as.Date("2011-06-01"), y=7.25,
+          label="Peaked Cash Rate before GFC") +
+  annotate(geom="text", x=as.Date("2006-10-01"), y=3.25,
+           label="Fifth consecutive rate cut") +
+  annotate(geom="text", x=as.Date("2014-12-15"), y=4.75,
+           label="China's growth started to slow down") +
+  annotate(geom="text", x=as.Date("2010-05-07"), y=2.5,
+           label="Mining investment has slowed down") +
+  annotate(geom="text", x=as.Date("2014-08-07"), y=1.5,
+           label="Start of a stable rate") +
+  coord_cartesian(clip = 'off') +
+  annotate(geom="point", x=as.Date("2008-09-30"), y=6, size=8, shape=21, fill="transparent") +
+  annotate(geom="point", x=as.Date("2008-08-01"), y=7.25, size=8, shape=21, fill="transparent") +
+  annotate(geom="point", x=as.Date("2009-02-01"), y=3.25, size=8, shape=21, fill="transparent") +
+  annotate(geom="point", x=as.Date("2011-10-01"), y=4.75, size=8, shape=21, fill="transparent") +
+  annotate(geom="point", x=as.Date("2013-08-07"), y=2.5, size=8, shape=21, fill="transparent") +
+  annotate(geom="point", x=as.Date("2016-08-07"), y=1.5, size=8, shape=21, fill="transparent") +
+  geom_smooth(method='lm') +
+  theme_ipsum()
+
+#Plotting yearly inflation rate
+ggplot(Combi_df, aes(date, yearly_inflation)) + geom_line() +
+  xlab("Date") + ylab("Yearly Inflation") + ggtitle("Year-ended Consumer Price Inflation") +
+  annotate(geom="text", x=as.Date("2011-08-30"), y=5,
+           label="GFC started affecting inflation") +
+  annotate(geom="text", x=as.Date("2012-02-28"), y=3.7,
+           label="Low confidence affected by GFC") +
+  annotate(geom="text", x=as.Date("2011-04-30"), y=1.2,
+           label="Credit growth") +
+  annotate(geom="text", x=as.Date("2014-08-30"), y=1,
+           label="Lowest inflation rate") +
+  coord_cartesian(clip = 'off') +
+  annotate(geom="point", x=as.Date("2008-11-30"), y=5, size=8, shape=21, fill="transparent") +
+  annotate(geom="point", x=as.Date("2009-02-28"), y=3.7, size=8, shape=21, fill="transparent") +
+  annotate(geom="point", x=as.Date("2009-11-30"), y=1.2, size=8, shape=21, fill="transparent") +
+  annotate(geom="point", x=as.Date("2016-08-30"), y=1, size=8, shape=21, fill="transparent") +
+  geom_smooth(method='lm') +
+  theme_ipsum()
+
+Combi_df[,c(1,9,10)]
+
+#Plotting quarterly and yearly inflation rate
+colnames(Combi_df)
+Infla <- Combi_df[,c(1,9,10)]
+Infla_long <- melt(Infla, id="date")  # convert to long format
+ggplot(data=Infla_long,
+       aes(x=date, y=value, colour=variable)) +
+  geom_line()
+
+# source https://stackoverflow.com/questions/3777174/plotting-two-variables-as-lines-using-ggplot2-on-the-same-graph
+
+
+}
+
+##### EDA post Feature Engineering ####
+Combi_eng_df <- data.frame(date=index(Combi_eng), coredata(Combi_eng))
+Combi_eng_df$date <- as.Date(Combi_eng_df$date)
+colnames(Combi_eng_df)
+#Plotting rba_cash_rate
+ggplot(Combi_eng_df, aes(date, rba_cash_rate)) + geom_line() +
+  xlab("Date") + ylab("RBA Cash Rate") + ggtitle("Cash Rate Target as announced by the RBA") +
+  #annotate(geom="text", x=as.Date("2012-01-01"), y=6,
+  #        label="Lehman Brothers filed for bankruptcy") +
+  # coord_cartesian(clip = 'off') +
+  #annotate(geom="point", x=as.Date("2008-09-30"), y=6, size=8, shape=21, fill="transparent") +
+  geom_smooth(method='lm') +
+  theme_ipsum()
+
+install.packages("reshape2")
+library(reshape2)
+
+colnames(Combi_eng_df)
+
+cluster1 <- Combi_eng_df[,c(1,2,11,12,16)]
+
+cluster1_long <- melt(cluster1, id="date")  # convert to long format
+
+colnames(cluster1_long)
+
+ggplot(data=cluster1_long,
+       aes(x=date, y=value, colour=variable)) +
+  geom_line()
+
+
+cluster2 <- Combi_eng_df[,c(1,3,13)]
+
+cluster2_long <- melt(cluster2, id="date")  # convert to long format
+
+colnames(cluster2_long)
+
+ggplot(data=cluster2_long,
+       aes(x=date, y=value, colour=variable)) +
+  geom_line()
+
+
+cluster3 <- Combi_eng_df[,c(1,8,9,10)]
+
+cluster3_long <- melt(cluster3, id="date")  # convert to long format
+
+colnames(cluster3_long)
+
+ggplot(data=cluster3_long,
+       aes(x=date, y=value, colour=variable)) +
+  geom_line()
+
+
+
+cluster4 <- Combi_eng_df[,c(1,4,5,6,14,15)]
+
+cluster4_long <- melt(cluster4, id="date")  # convert to long format
+
+colnames(cluster4_long)
+
+ggplot(data=cluster4_long,
+       aes(x=date, y=value, colour=variable)) +
+  geom_line()
+
+create_report(Combi_eng_df)
+
